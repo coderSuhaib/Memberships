07ed90d88b1972ffb079a80cedb25ab6cfbaf08f
Suhaib Al-tamimi <suhaib.altamimi3@gmail.com>
2018-06-04 23:48:20 -0700

    Add project files.

---
+++ Memberships/Entities/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    // I'm using this dataAnnotation "Table" so when the automatic data migration happends it will no use the plural 
+    // name of the class "sections"
+    [Table("Section")]
+    public class Section
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+    }
+}
